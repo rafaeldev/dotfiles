666127c0c06cdfdd9ab0d2b969928edb52995dc7
Rafael Gomes Francisco <rafael.dev@hotmail.com>
2018-11-26 10:29:12 -0200

    deepin personal bindings

---
+++ com.deepin.wrap.gnome.mutter.keybindings.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+gsettings set com.deepin.wrap.gnome.mutter.keybindings toggle-tiled-right "['<Super>Right']"
+gsettings set com.deepin.wrap.gnome.mutter.keybindings toggle-tiled-left "['<Super>Left']"
+gsettings set com.deepin.wrap.gnome.mutter.keybindings tab-popup-cancel "['']"
+gsettings set com.deepin.wrap.gnome.mutter.keybindings tab-popup-select "['']"
